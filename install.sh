d1cd3013b59ddd0ce83a4cb7e3cf6f75ecb8867e
fishsticksnom <121593097+fishsticksnom@users.noreply.github.com>
2023-02-16 23:48:02 -0500

    Add install packages script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install bspwm
+sudo apt install dunst
+sudo apt install dunst
+sudo apt install kitty
+sudo apt install picom
+sudo apt install polybar
+sudo apt install rofi
+sudo apt install scrot
+sudo apt install sxhkd
